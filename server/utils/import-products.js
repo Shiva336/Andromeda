e1ce353d1736676415e328c1a987044cfeb6789a
Deslo1 <ravisankarmenon2001@gmail.com>
2023-05-25 16:08:22 +0530

    data added to backend

--- server/utils/import-products.js
+++ server/utils/import-products.js
@@ -9,36 +9,40 @@ const url = 'mongodb+srv://admin:admin@andromeda.esfay3s.mongodb.net/andromeda';
 const { config } = require('dotenv');
 
 config({ path: '../config/config.env' });
-mongoose.set('strictQuery', false);
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection.once('open', () => {
-  console.log('connected');
-});
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
 const product = JSON.parse(fs.readFileSync(`./product-data.json`, 'utf-8'));
 // IMPORT DATA INTO DB
-console.log(product.length);
 const importData = async () => {
   try {
-    for (i = 0; i < product.length; i++) {
-      const newProduct = new productModel({
-        name: product[i].name,
-        highlights: product[i].highlights,
-        category: product[i].category,
-        price: product[i].price,
-        img: product[i].img,
-        featured: product[i].featured,
-      });
-
-      const Product = await newProduct.save();
-    }
+    await productModel.create(product);
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
+  process.exit();
 };
 
-importData();
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await productModel.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
