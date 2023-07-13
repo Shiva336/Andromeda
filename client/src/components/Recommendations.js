b02ce9008c4a907bf612679c41ab37e8d59091ea
IN-Accurate <vishalsankar410@gmail.com>
2023-07-13 15:39:00 +0530

    Implement caption generation

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -1,8 +1,54 @@
 import '../styles/Recommendation.css';
-// import axios from 'axios';
-// import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function Recommendations() {
+  const [prompt, setPrompt] = useState('');
+  const [caption, setCaption] = useState('');
+  var flag = 0;
+  async function getData() {
+    if (flag !== 0) {
+      try {
+        setPrompt(
+          'Create a catchy caption (of 6 words) for an ad for a t-shirt used by american man age 20'
+        );
+        const response = await axios.post(
+          'https://api.openai.com/v1/chat/completions',
+          {
+            model: 'gpt-3.5-turbo',
+            messages: [
+              {
+                role: 'user',
+                content: prompt,
+              },
+            ],
+            temperature: 1,
+            max_tokens: 128,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer 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`,
+            },
+          }
+        );
+        const generatedCaption = response.data.choices[0].message.content;
+        setCaption(generatedCaption);
+        console.log(caption);
+      } catch (error) {
+        console.error('Error generating caption:', error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      await getData();
+    })();
+  }, []);
+
   return (
     <div className='recommendation-div' style={{ overflow: 'hidden' }}>
       <img
@@ -16,46 +62,9 @@ function Recommendations() {
       <img
         alt='test'
         src='http://image.pollinations.ai/prompt/image%20of%20%20american%20man%20age%2020%20using%20hp%20laptop'
-      />
+      />{' '}
     </div>
   );
 }
 
 export default Recommendations;
-
-/*
-  async function getData() {
-    const options = {
-      method: 'POST',
-      url: 'https://openai80.p.rapidapi.com/chat/completions',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '51494e2018mshea07d648334dbd1p187269jsn1a609db27914',
-        'X-RapidAPI-Host': 'openai80.p.rapidapi.com',
-      },
-      data: {
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'user',
-            content:
-              'Write me a catchy and short caption for an ad of a rolex watch',
-          },
-        ],
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  useEffect(() => {
-    (async () => {
-      await getData();
-    })();
-    return () => {};
-  }, []);
-  */
