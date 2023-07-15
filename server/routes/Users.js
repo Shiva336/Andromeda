addb34a7c0ab171c98e71d41fa1b7f1ca015c9d4
IN-Accurate <vishalsankar410@gmail.com>
2023-07-16 02:02:51 +0530

    Create string template for ad,create user search route in backend

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,58 +1,62 @@
-const router = require("express").Router();
-const userModel = require("../models/users");
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const userModel = require('../models/users');
+const bcrypt = require('bcrypt');
 
 //update
-router.put("/:id",async (req,res)=> {
-  if(req.body.uid === req.params.uid || req.body.isAdmin) {
+router.put('/:id', async (req, res) => {
+  if (req.body.uid === req.params.uid || req.body.isAdmin) {
     try {
       const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password,salt);
-    }
-    catch(error) {
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    } catch (error) {
       return res.status(500).json(error);
     }
 
     try {
-      const user = await userModel.findByIdAndUpdate(req.params.id,{ $set: req.body });
-      res.status(200).json("Account updated");
-    }
-    catch(err) {
+      const user = await userModel.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('Account updated');
+    } catch (err) {
       return res.json(err);
     }
-  }
-  else {
-    return res.status(403).json("you can delete only your account");
+  } else {
+    return res.status(403).json('you can delete only your account');
   }
 });
 
-//delete 
-router.delete("/:id",async (req,res)=> {
-  if(req.body.uid === req.params.uid || req.body.isAdmin) {
+//delete
+router.delete('/:id', async (req, res) => {
+  if (req.body.uid === req.params.uid || req.body.isAdmin) {
     try {
       const user = await userModel.findByIdAndDelete(req.params.id);
       res.status(200).json(user);
-    }
-    catch(error) {
+    } catch (error) {
       return res.status(500).json(error);
     }
-  }
-  else {
-    return res.status(403).json("you can delete only your account");
+  } else {
+    return res.status(403).json('you can delete only your account');
   }
 });
 
 //get a user
-router.get("/:id", async (req,res)=> {
-  try{
-    const user =  await userModel.findById(req.params.id);
-    const {password,updatedAt, ...other} = user._doc; //what is is this line?
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc; //what is is this line?
     res.status(200).json(other);
-  }
-  catch(err) {
+  } catch (err) {
     return res.status(500).json(error);
   }
 });
 
+router.post('/search-by-name', async (req, res) => {
+  try {
+    let user = await userModel.findOne({ name: req.body.name });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(200).json("user doesn't exist");
+  }
+});
 
 module.exports = router;
