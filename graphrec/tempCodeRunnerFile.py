f1568e30d6991e44705df3d543c2918a11020cb3
Deslo1 <ravisankarmenon2001@gmail.com>
2023-07-16 14:25:45 +0530

    almost ready

--- graphrec/tempCodeRunnerFile.py
+++ graphrec/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-   file_path = './data/model_parameters1.pth'
-    device = torch.device('cpu')
-    if os.path.exists(file_path):
-        graphrec.load_state_dict(torch.load(file_path, map_location=device))
-        print("Loaded model parameters from:", file_path)
+e_
