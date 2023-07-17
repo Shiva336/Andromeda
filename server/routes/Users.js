a6e95d9d60b9b12478d8d9401f91a1b3dd1828ad
Shiva Sundar <shivasundar911@gmail.com>
2023-07-17 11:49:47 +0530

    pull

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -57,15 +57,9 @@ router.post('/search-by-name', async (req, res) => {
     let user = await userModel.findOne({ name: req.body.name });
     let top3 = [];
     const arr = user.top3;
-<<<<<<< HEAD
-    for(let i=0; i<top3.length; i++) {      
-      let temp = await userModel.findOne({index: arr[i]});
-      top3.push(temp)
-=======
     for (let i = 0; i < arr.length; i++) {
       let temp = await productModel.findOne({ index: arr[i] });
       top3.push(temp);
->>>>>>> 0eab150044d59c9d53e39f896e48db0688f15901
     }
     res.status(200).json({ user: user, top3: top3 });
   } catch (err) {
@@ -73,8 +67,6 @@ router.post('/search-by-name', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-=======
 router.post('/top3', async (req, res) => {
   try {
     console.log(req.body.top3);
@@ -89,6 +81,5 @@ router.post('/top3', async (req, res) => {
     res.status(200).json("user doesn't exist");
   }
 });
->>>>>>> 0eab150044d59c9d53e39f896e48db0688f15901
 
 module.exports = router;
